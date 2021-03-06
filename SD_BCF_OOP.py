1c6c2f984ea12ed2c4e1352ed7359e4744e0a0ef
Mohamed <moelrefaiy@gmail.com>
2021-03-05 19:50:08 -0600

    optimize correlation function

--- SD_BCF_OOP.py
+++ SD_BCF_OOP.py
@@ -52,7 +52,7 @@ class CorrelationFunction:
         pass
 
     def plotting(self, x_axis):
-        plt.plot(x_axis, self.calculate()  # ex: SpectralDensity.plotting(over,omega_list)
+        plt.plot(x_axis, self.calculate())  # ex: SpectralDensity.plotting(over,omega_list)
 
 
 class COverDamped(CorrelationFunction):
@@ -159,48 +159,48 @@ omega_c = 500  # Unit: cm^-1
 l = 0
 N = 40
 
-# Object Instantiation:
-# ---------------------
-SD = SpectralDensity(omega_list)  # SD Object Instantiation
-C_t = CorrelationFunction(omega_list, lamd, gamma, omega_c, kBT)  # correlation Object Instantiation
-MatsubaraApproximation = MatsubaraApproximation(omega_list, lamd, gamma, omega_c)  # Matsubara Approx Object
-# Instantiation
-
-# storing list:
-# -------------
-BCF_under_damped_highTemp = [ ]
-BCF_over_damped_highTemp = [ ]
-BCF_under_damped_lowTemp = [ ]
-BCF_over_damped_lowTemp = [ ]
-
-for t in t_list:
-    # High temp approximation:
-    # ------------------------
-    BCF_under_damped_highTemp.append(C_t.correlation_underDamped_mode(t))
-    BCF_over_damped_highTemp.append(C_t.correlation_overDamped_mode(t))
-    # Low temperature approximation:
-    # ------------------------------
-    BCF_over_damped_lowTemp.append(
-        C_t.correlation_overDamped_mode(t) - MatsubaraApproximation.Mats_overDamped(t, l, N))
-    BCF_under_damped_lowTemp.append(
-        C_t.correlation_underDamped_mode(t) - MatsubaraApproximation.Mats_underDamped(t, l, N))
-
-Total_BCF_highTemp = np.array(BCF_under_damped_highTemp) + np.array(BCF_over_damped_highTemp)
-Total_BCF_lowTemp = np.array(BCF_under_damped_lowTemp) + np.array(BCF_over_damped_lowTemp)
-
-Total_SD = SD.J_overDamped_mode() + SD.J_underDamped_mode(omega_c)  # construct 1 over & 1 under-damped mode
-
-# Plotting:
-# ---------
-# plt.plot(t_list, Total_BCF_highTemp)
-# plt.plot(t_list,Total_BCF_lowTemp)
-# plt.plot(Total_SD)
-# plt.plot(t_list, BCF_over_damped_lowTemp)
-# plt.plot(t_list, BCF_under_damped_lowTemp)
+# # Object Instantiation:
+# # ---------------------
+# SD = SpectralDensity(omega_list)  # SD Object Instantiation
+# C_t = CorrelationFunction(omega_list, lamd, gamma, omega_c, kBT)  # correlation Object Instantiation
+# MatsubaraApproximation = MatsubaraApproximation(omega_list, lamd, gamma, omega_c)  # Matsubara Approx Object
+# # Instantiation
+#
+# # storing list:
+# # -------------
+# BCF_under_damped_highTemp = [ ]
+# BCF_over_damped_highTemp = [ ]
+# BCF_under_damped_lowTemp = [ ]
+# BCF_over_damped_lowTemp = [ ]
+#
+# for t in t_list:
+#     # High temp approximation:
+#     # ------------------------
+#     BCF_under_damped_highTemp.append(C_t.correlation_underDamped_mode(t))
+#     BCF_over_damped_highTemp.append(C_t.correlation_overDamped_mode(t))
+#     # Low temperature approximation:
+#     # ------------------------------
+#     BCF_over_damped_lowTemp.append(
+#         C_t.correlation_overDamped_mode(t) - MatsubaraApproximation.Mats_overDamped(t, l, N))
+#     BCF_under_damped_lowTemp.append(
+#         C_t.correlation_underDamped_mode(t) - MatsubaraApproximation.Mats_underDamped(t, l, N))
+#
+# Total_BCF_highTemp = np.array(BCF_under_damped_highTemp) + np.array(BCF_over_damped_highTemp)
+# Total_BCF_lowTemp = np.array(BCF_under_damped_lowTemp) + np.array(BCF_over_damped_lowTemp)
+#
+# Total_SD = SD.J_overDamped_mode() + SD.J_underDamped_mode(omega_c)  # construct 1 over & 1 under-damped mode
+#
+# # Plotting:
+# # ---------
+# # plt.plot(t_list, Total_BCF_highTemp)
+# # plt.plot(t_list,Total_BCF_lowTemp)
+# # plt.plot(Total_SD)
+# # plt.plot(t_list, BCF_over_damped_lowTemp)
+# # plt.plot(t_list, BCF_under_damped_lowTemp)
+# # plt.show()
+#
+# fig, axs = plt.subplots(2)
+# axs[ 0 ].plot(t_list, Total_BCF_highTemp, 'g')
+# # axs[ 1 ].plot(t_list,Total_BCF_lowTemp, 'r')
+# axs[ 1 ].plot(Total_SD, 'g--')
 # plt.show()
-
-fig, axs = plt.subplots(2)
-axs[ 0 ].plot(t_list, Total_BCF_highTemp, 'g')
-# axs[ 1 ].plot(t_list,Total_BCF_lowTemp, 'r')
-axs[ 1 ].plot(Total_SD, 'g--')
-plt.show()
