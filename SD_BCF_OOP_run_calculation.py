6e4c9dccf1da8ccf8f7e06a7f9abdfff00403c44
Mohamed <moelrefaiy@gmail.com>
2021-03-06 00:01:21 -0600

    optimize run code

--- SD_BCF_OOP_run_calculation.py
+++ SD_BCF_OOP_run_calculation.py
@@ -1,6 +1,8 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
-import SD_BCF_OOP as SCP
+from SD_BCF_OOP import SpectralDensity, Spectral_OverDamped, Spectral_UnderDamped, CorrelationFunction, \
+    Correlation_overDamped, Correlation_underDamped
 
 # Constants:
 # ----------
@@ -8,63 +10,45 @@ T = 295  # Unit : K
 kBT = 0.69503 * T  # Unit: cm^-1
 h_bar = 5308  # Unit: cm^-1 * fs
 gamma = 100  # Unit: cm^-1
-lamd = 50  # Unit : cm^-1
 delta_t = 5  # Unit:fs
 t_list = np.arange(delta_t, 1000, delta_t)  # Unit : fs
 delta_omega = 1  # Unit: cm^-1
 omega_list = np.arange(delta_omega, 1000, delta_omega)  # Unit: cm^-1
-omega_c = 500  # Unit: cm^-1
+# omega_c = 500  # Unit: cm^-1
+# lamd = 50  # Unit : cm^-1
+omega_c = np.array([ [ 300 ], [ 518 ], [ 745 ] ])  # Unit: cm^-1
+lamd = np.array([ [ 161 ], [ 16 ], [ 48 ] ])  # Unit : cm^-1
+
 l = 0
 N = 40
 
-# Object Instantiation:
-# ---------------------
-SD = SCP.SpectralDensity()  # SD Object Instantiation
-# C_t = SCP.CorrelationFunction(omega_list, lamd, gamma, omega_c, kBT)  # correlation Object Instantiation
-# MatsubaraApproximation = SCP.MatsubaraApproximation(omega_list, lamd, gamma, omega_c)  # Matsubara Approx Object
-# Instantiation
+# # Object Instantiation:
+# # ---------------------
+SD = SpectralDensity(lamd, gamma, kBT, h_bar)
+SD_over1 = Spectral_OverDamped(omega_list, omega_c[ 0 ], lamd, gamma, kBT, h_bar)  # the first over-damped
+SD_over2 = Spectral_OverDamped(omega_list, omega_c[ 1 ], lamd, gamma, kBT, h_bar)  # the second over-damped
+SD_under1 = Spectral_UnderDamped(omega_list, omega_c[ 0 ], lamd, gamma, kBT, h_bar)  # the first under-damped
+SD_under2 = Spectral_UnderDamped(omega_list, omega_c[ 1 ], lamd, gamma, kBT, h_bar)  # the second under-damped
+SD_under3 = Spectral_UnderDamped(omega_list, omega_c[ 2 ], lamd, gamma, kBT, h_bar)  # the second under-damped
 
-# storing list:
-# -------------
-BCF_under_damped_highTemp = [ ]
-BCF_over_damped_highTemp = [ ]
-BCF_under_damped_lowTemp = [ ]
-BCF_over_damped_lowTemp = [ ]
+# Mats_over = Mats_overDamped(omega_list, omega_c, t_list, l, N)
+# Mats_under = Mats_underDamped(omega_list, omega_c, t_list, l, N)
 
-# for t in t_list:
-#     # High temp approximation:
-#     # ------------------------
-#     BCF_under_damped_highTemp.append(C_t.correlation_underDamped_mode(t))
-#     BCF_over_damped_highTemp.append(C_t.correlation_overDamped_mode(t))
-#     # Low temperature approximation:
-#     # ------------------------------
-#     BCF_over_damped_lowTemp.append(
-#         C_t.correlation_overDamped_mode(t) - MatsubaraApproximation.Mats_overDamped(t, l, N))
-#     BCF_under_damped_lowTemp.append(
-#         C_t.correlation_underDamped_mode(t) - MatsubaraApproximation.Mats_underDamped(t, l, N))
-
-# Total_BCF_highTemp = np.array(BCF_under_damped_highTemp) + np.array(BCF_over_damped_highTemp)
-# Total_BCF_lowTemp = np.array(BCF_under_damped_lowTemp) + np.array(BCF_over_damped_lowTemp)
-
-Total_SD = SCP.SdUnderDamped(omega_list, omega_c) + SCP.SdOverDamped(omega_c)  # construct 1 over & 1 under-damped mode
-
-# Plotting:
-# ---------
-# plt.plot(t_list, Total_BCF_highTemp)
-# plt.plot(t_list,Total_BCF_lowTemp)
-# plt.plot(Total_SD)
-# plt.plot(t_list, BCF_over_damped_lowTemp)
-# plt.plot(t_list, BCF_under_damped_lowTemp)
+Total_SD = SD_over1.calculate() + SD_under1.calculate() + SD_under2.calculate() + SD_under3.calculate()  # construct 1 over & 1 under-damped mode
+Total_SD = Total_SD.reshape(999, 3)
+# plt.plot(omega_list, Total_SD)
 # plt.show()
 
-# fig, axs = plt.subplots(2)
-# axs[ 0 ].plot(t_list, Total_BCF_highTemp, 'g')
-# # axs[ 1 ].plot(t_list,Total_BCF_lowTemp, 'r')
-# axs[ 1 ].plot(Total_SD, 'g--')
-# plt.show()
+Correlation = CorrelationFunction(lamd, gamma, kBT, h_bar)
 
+corr_over1 = Correlation_overDamped(omega_list, 60, t_list, lamd, gamma, kBT, h_bar)
+corr_over2 = Correlation_overDamped(omega_list, 70, t_list, lamd, gamma, kBT, h_bar)
+corr_under1 = Correlation_underDamped(omega_list, omega_c[ 0 ], t_list, lamd, gamma, kBT, h_bar)
+corr_under2 = Correlation_underDamped(omega_list, omega_c[ 1 ], t_list, lamd, gamma, kBT, h_bar)
+corr_under3 = Correlation_underDamped(omega_list, omega_c[ 2 ], t_list, lamd, gamma, kBT, h_bar)
 
-# SD_under.plotting(omega_list)
-# CR = CorrelationFunction(lamd,gamma,kBT,h_bar)
-# CR_over = COverDamped(omega_list,omega_c)
-# CR_over.calculate()
+Total_correlation = corr_over1.calculate() + corr_over2.calculate() + corr_under1.calculate() + corr_under2.calculate() + corr_under3.calculate()
+Total_correlation = Total_correlation.reshape(199, 3)
+
+plt.plot(t_list, Total_correlation)
+plt.show()
